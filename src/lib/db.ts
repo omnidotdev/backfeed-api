9b0e5a825af8804bbfac60b6571e0b2c96f22fbc
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-10-19 01:18:47 -0500

    refactor: remove prisma, send it on drizzle

--- src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-/**
- * Prisma database client.
- */
-const db = new PrismaClient();
-
-export default db;
--- src/lib/drizzle/db.ts
+++ src/lib/drizzle/db.ts
@@ -2,10 +2,11 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 import { DATABASE_URL } from "lib/config/env";
+import * as schema from "lib/drizzle/schema";
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
 });
 
-// TODO: add schema
-export const db = drizzle(pool);
+// NB: this is just for a ref if needed. Since drizzle and postgraphile v5 depend on connection strings, there is no (current) need for an explicit pool. Good for scripting though.
+export const db = drizzle(pool, { schema });
