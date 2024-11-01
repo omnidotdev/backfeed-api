1d32cf51df3dc0003146c8e265d9b16672679dc8
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-10-31 19:29:49 -0500

    Revert "refactor(scripts): modularize DB seed script"
    This reverts commit cdd18988344dc8ccf453f1ae26a237db1aa95d97.

--- src/scripts/seedDatabase.ts
+++ src/scripts/seedDatabase.ts
@@ -12,67 +12,58 @@ import {
   usersToOrganizations,
 } from "lib/drizzle/schema";
 
-import type { TableConfig, TablesRelationalConfig } from "drizzle-orm";
-import type { NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
-import type { PgTable, PgTransaction } from "drizzle-orm/pg-core";
+import type {
+  InsertOrganization,
+  InsertPost,
+  InsertProject,
+  InsertUser,
+  InsertUpvote,
+} from "lib/drizzle/schema";
 
 /**
  * Seed a database entity with sample data.
  */
-const seedEntity = async <
-  TFullSchema extends Record<string, unknown>,
-  TSchema extends TablesRelationalConfig,
->(
-  /** Transaction to run the seed in. */
-  tx: PgTransaction<NodePgQueryResultHKT, TFullSchema, TSchema>,
-  /*** Database entity to seed. */
-  entity: PgTable<TableConfig>,
-  /** Custom seed logic. */
-  seedFn: (
-    tx: PgTransaction<NodePgQueryResultHKT, TFullSchema, TSchema>
-    // TODO type properly
-    // biome-ignore lint/suspicious/noExplicitAny: tricky to type
-  ) => any[] | Promise<any[]>
-) => {
-  // delete all entities
-  await tx.delete(entity);
-
-  const newEntities = await seedFn(tx);
-
-  // insert new entities
-  await tx.insert(entity).values(newEntities);
-
-  return newEntities;
-};
-
-/**
- * Seed the database with sample data.
- * ! NB: only run this script in development
- */
 const seedDatabase = async () => {
+  // ! NB: only run this script in development
   if (!isDev || !DATABASE_URL?.includes("localhost")) {
-    console.error("This script can only be run in development.");
+    console.log("This script can only be run in development");
     process.exit(1);
   }
 
   console.log("Seeding database...");
 
   await dbPool.transaction(async (tx) => {
-    const newUsers = await seedEntity(tx, users, () =>
-      Array.from({ length: 10 }, () => ({
+    // users
+    await tx.delete(users);
+
+    const newUsers: InsertUser[] = [];
+    for (let i = 0; i < 10; i++) {
+      newUsers.push({
         // hell yeah
         walletAddress: faker.finance.ethereumAddress(),
-      }))
-    );
+      });
+    }
 
-    const newOrganizations = await seedEntity(tx, organizations, () =>
-      Array.from({ length: 10 }, () => ({
+    await tx.insert(users).values(newUsers);
+
+    // organizations
+    await tx.delete(organizations);
+
+    const newOrganizations: InsertOrganization[] = [];
+    for (let i = 0; i < 10; i++) {
+      newOrganizations.push({
         name: faker.company.name(),
         slug: faker.lorem.slug(),
-      }))
-    );
+      });
+    }
 
-    const newProjects = await seedEntity(tx, projects, async () => {
+    await tx.insert(organizations).values(newOrganizations);
+
+    // projects
+    await tx.delete(projects);
+
+    const newProjects: InsertProject[] = [];
+    for (let i = 0; i < 10; i++) {
       const randomOrganization =
         newOrganizations[Math.floor(Math.random() * newOrganizations.length)];
 
@@ -81,27 +72,32 @@ const seedDatabase = async () => {
         .from(organizations)
         .where(eq(organizations.name, randomOrganization.name!));
 
-      return Array.from({ length: 10 }, (_, idx) => ({
+      newProjects.push({
         // NB: using the index due to the unique constraint in the projects table
-        name: `${faker.company.buzzVerb()}-${idx}`,
+        name: `${faker.company.buzzVerb()}-${i}`,
         description: faker.lorem.paragraph(),
         slug: faker.lorem.slug(),
         image: faker.image.avatar(),
         organizationId: selectedOrganization.id,
-      }));
-    });
+      });
+    }
 
-    const newPosts = await seedEntity(tx, posts, async () => {
+    await tx.insert(projects).values(newProjects);
+
+    // posts
+    await tx.delete(posts);
+
+    const newPosts: InsertPost[] = [];
+
+    for (let i = 0; i < 10; i++) {
       const randomProject =
         newProjects[Math.floor(Math.random() * newProjects.length)];
-
       const randomUser = newUsers[Math.floor(Math.random() * newUsers.length)];
 
       const [selectedProject] = await tx
         .select()
         .from(projects)
         .where(eq(projects.name, randomProject.name!));
-
       const [selectedUser] = await tx
         .select()
         .from(users)
@@ -127,38 +123,41 @@ const seedDatabase = async () => {
         });
       }
 
-      return Array.from({ length: 10 }, () => ({
+      newPosts.push({
         title: `${faker.commerce.productAdjective()} ${faker.commerce.product()}`,
         description: faker.lorem.paragraph(),
         projectId: selectedProject.id,
         userId: selectedUser.id,
-      }));
-    });
+      });
+    }
 
-    await seedEntity(tx, upvotes, () =>
-      Array.from({ length: 10 }, async (_, idx) => {
-        // NB: not randomizing the user here due to the unique constraint in the upvotes table
-        const user = newUsers[idx];
+    await tx.insert(posts).values(newPosts);
 
-        const randomPost =
-          newPosts[Math.floor(Math.random() * newUsers.length)];
+    // upvotes
+    await tx.delete(upvotes);
 
-        const [selectedUser] = await tx
-          .select()
-          .from(users)
-          .where(eq(users.walletAddress, user.walletAddress!));
+    const newUpvotes: InsertUpvote[] = [];
+    for (let i = 0; i < 10; i++) {
+      // NB: not randomizing the user here due to the unique constraint in the upvotes table
+      const user = newUsers[i];
+      const randomPost = newPosts[Math.floor(Math.random() * newUsers.length)];
 
-        const [selectedPost] = await tx
-          .select()
-          .from(posts)
-          .where(eq(posts.title, randomPost.title!));
+      const [selectedUser] = await tx
+        .select()
+        .from(users)
+        .where(eq(users.walletAddress, user.walletAddress!));
+      const [selectedPost] = await tx
+        .select()
+        .from(posts)
+        .where(eq(posts.title, randomPost.title!));
 
-        return {
-          postId: selectedPost.id,
-          userId: selectedUser.id,
-        };
-      })
-    );
+      newUpvotes.push({
+        postId: selectedPost.id,
+        userId: selectedUser.id,
+      });
+    }
+
+    await tx.insert(upvotes).values(newUpvotes);
   });
 
   console.log("Database has been seeded successfully!");
