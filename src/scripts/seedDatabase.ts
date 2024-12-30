cbcdfeeff35544411124a3270c9b5b42300d4564
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-12-30 17:01:47 -0600

    Update Seed Script (#24)
    * refactor(scripts): update seed script to properly scope post dates
    
    * refactor(scripts): update seed script to provide a wider range of entries to test
    
    * chore: add new lines to seed script
    
    * Refactor(scripts): adjust seed script to update project descriptions
    
    * refactor(scripts): update migration script to update executable schema
    
    * build: update drizzle-seed dependency
    
    * refactor(scripts): update seed script to handle dynamic createdAt dates for posts

--- src/scripts/seedDatabase.ts
+++ src/scripts/seedDatabase.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { reset, seed } from "drizzle-seed";
 
@@ -18,13 +19,45 @@ const seedDatabase = async () => {
   await reset(db, schema);
 
   console.log("Seeding database...");
+
   await seed(db, schema).refine((f) => ({
     organizations: {
+      count: 400,
       columns: {
         name: f.companyName(),
       },
     },
+    projects: {
+      count: 2000,
+      columns: {
+        description: f.loremIpsum(),
+        slug: f.string({
+          isUnique: true,
+        }),
+      },
+    },
+    posts: {
+      count: 10000,
+      columns: {
+        title: f.companyName(),
+        description: f.loremIpsum(),
+        createdAt: f.date({
+          minDate: dayjs(new Date()).subtract(7, "day").format("YYYY-MM-DD"),
+          maxDate: dayjs(new Date()).format("YYYY-MM-DD"),
+        }),
+      },
+    },
+    users: {
+      count: 500,
+    },
+    usersToOrganizations: {
+      count: 50,
+    },
+    upvotes: {
+      count: 1000,
+    },
   }));
+
   console.log("Database has been seeded successfully!");
 };
 
