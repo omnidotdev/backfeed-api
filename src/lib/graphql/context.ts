2860c782c142d5d77e605a7e1fe8700a5b4f8d65
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-16 17:02:08 -0600

    refactor(plugins): update useGenericAuth to use jose for JWT handling

--- src/lib/graphql/context.ts
+++ src/lib/graphql/context.ts
@@ -1,6 +1,6 @@
 import { createWithPgClient } from "@dataplan/pg/adaptors/pg";
 
-import { dbClient } from "lib/db/db";
+import { dbPool } from "lib/db/db";
 import { pgPool } from "lib/db/pool";
 
 import type { YogaInitialContext } from "graphql-yoga";
@@ -10,7 +10,7 @@ import type { NodePostgresPgClient } from "postgraphile/adaptors/pg";
 const withPgClient = createWithPgClient({ pool: pgPool });
 
 export interface GraphQLContext {
-  db: typeof dbClient;
+  db: typeof dbPool;
   request: Request;
   withPgClient: WithPgClient<NodePostgresPgClient>;
 }
@@ -18,7 +18,7 @@ export interface GraphQLContext {
 export const createGraphQLContext = async ({
   request,
 }: YogaInitialContext): Promise<GraphQLContext> => ({
-  db: dbClient,
+  db: dbPool,
   request,
   withPgClient,
 });
--- src/lib/plugins/useGenericAuth.ts
+++ src/lib/plugins/useGenericAuth.ts
@@ -1,80 +1,53 @@
 import { useGenericAuth as useEnvelopGenericAuth } from "@envelop/generic-auth";
-import jwt from "jsonwebtoken";
-import jwksClient from "jwks-rsa";
+import * as jose from "jose";
 
 import { AUTH_JWKS_URL } from "lib/config/env";
 import { users } from "lib/drizzle/schema";
 
 import type { ResolveUserFn } from "@envelop/generic-auth";
-import type { JwtHeader, SigningKeyCallback } from "jsonwebtoken";
 import type { SelectUser } from "lib/drizzle/schema";
 import type { GraphQLContext } from "lib/graphql";
-
-type Maybe<T> = T | null;
 
 const resolveUser: ResolveUserFn<SelectUser, GraphQLContext> = async (
   context
 ) => {
   try {
-    let user: Maybe<SelectUser> = null;
-
     const sessionToken = context.request.headers
       .get("authorization")
       ?.split("Bearer ")[1];
 
     if (!sessionToken) throw new Error("Invalid or missing session token");
 
-    const jwks = jwksClient({
-      jwksUri: AUTH_JWKS_URL!,
-    });
+    const jwks = jose.createRemoteJWKSet(new URL(AUTH_JWKS_URL!));
 
-    const getKey = (header: JwtHeader, callback: SigningKeyCallback) => {
-      jwks.getSigningKey(header.kid, (err, key) => {
-        if (err) {
-          console.error(err);
-        }
+    const { payload } = await jose.jwtVerify(sessionToken, jwks);
 
-        const signingKey = key?.getPublicKey();
-        callback(null, signingKey);
-      });
-    };
+    if (!payload) throw new Error("Invalid or missing session token");
 
-    jwt.verify(
-      sessionToken,
-      getKey,
-      { algorithms: ["RS256"] },
-      async (err, decoded) => {
-        if (err) throw new Error("Failed to verify session token");
+    const [upsertedUser] = await context.db
+      .insert(users)
+      .values({
+        // @ts-ignore TODO: figure out why there is a type error here
+        hidraId: payload.sub!,
+        username: payload.preferred_username,
+        firstName: payload.given_name,
+        lastName: payload.family_name,
+      })
+      .onConflictDoUpdate({
+        target: users.hidraId,
+        set: {
+          username: payload.preferred_username as string,
+          firstName: payload.given_name as string,
+          lastName: payload.family_name as string,
+          updatedAt: new Date().toISOString(),
+        },
+      })
+      .returning();
 
-        const decodedToken = decoded as jwt.JwtPayload;
-
-        // upsert user into database
-        const [upsertedUser] = await context.db
-          .insert(users)
-          .values({
-            hidraId: decodedToken.sub!,
-            username: decodedToken.preferred_username,
-            firstName: decodedToken.given_name,
-            lastName: decodedToken.family_name,
-          })
-          .onConflictDoUpdate({
-            target: users.hidraId,
-            set: {
-              username: decodedToken.preferred_username,
-              firstName: decodedToken.given_name,
-              lastName: decodedToken.family_name,
-              updatedAt: new Date().toISOString(),
-            },
-          })
-          .returning();
-
-        user = upsertedUser;
-      }
-    );
-
-    return user;
+    return upsertedUser;
   } catch (e) {
-    console.error("Failed to validate token");
+    // TODO: generalize error message?
+    console.error(e);
 
     return null;
   }
