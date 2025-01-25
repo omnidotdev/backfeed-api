40f6afc76e6ad7915bf33cf4d33ac25d8bafcbe3
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-25 10:57:40 -0600

    OIDC Authentication (#28)
    * build(deps): add hono oidc auth package
    
    * refactor(server): allow GET methods
    
    * build(deps): remove hono auth package, add generic auth envelop plugin package
    
    * feature(plugins): add useGenericAuth plugin WIP
    
    * chore: remove ts-ignore
    
    * refactor: add envs for AUTH_JWKS_URL, remove specific logs
    
    * refactor(plugins): update useGenericAuth to use jose for JWT handling
    
    * refactor(plugins): rename upsertedUser --> user
    
    * refactor(plugins): update naming convention, useGenericAuth --> useAuth
    
    * chore(plugins): update file name, useGenericAuth --> useAuth
    
    * refactor(plugins): update useAuth to dedup claims derived from payload
    
    * refactor(plugins): remove upsert from auth flow
    
    * chore(plugin): revert changes, continuw with upsert
    
    * docs: add JSDoc to createGraphQLContext function
    
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>
    
    * refactor(plugins): remove TODO, update catch block pattern
    
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>
    
    * docs: add JSDoc to resolveUser
    
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>

---
+++ src/lib/graphql/context.ts
@@ -0,0 +1,28 @@
+import { createWithPgClient } from "@dataplan/pg/adaptors/pg";
+
+import { dbPool } from "lib/db/db";
+import { pgPool } from "lib/db/pool";
+
+import type { YogaInitialContext } from "graphql-yoga";
+import type { WithPgClient } from "postgraphile/@dataplan/pg";
+import type { NodePostgresPgClient } from "postgraphile/adaptors/pg";
+
+const withPgClient = createWithPgClient({ pool: pgPool });
+
+export interface GraphQLContext {
+  db: typeof dbPool;
+  request: Request;
+  withPgClient: WithPgClient<NodePostgresPgClient>;
+}
+
+/**
+ * Create a GraphQL context.
+ * @see https://graphql.org/learn/execution/#root-fields-and-resolvers
+ */
+export const createGraphQLContext = async ({
+  request,
+}: YogaInitialContext): Promise<GraphQLContext> => ({
+  db: dbPool,
+  request,
+  withPgClient,
+});
