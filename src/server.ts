5ff6989f583b3925a3e470c187dfac1816bcfd3c
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-30 22:57:03 -0600

    feature(middleware): add appropriate auth middleware to checkout and portal endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { Checkout, CustomerPortal, Webhooks } from "@polar-sh/hono";
 import { useGrafast, useMoreDetailedErrors } from "grafast/envelop";
 import { createYoga } from "graphql-yoga";
-import { Hono } from "hono";
+import { type Context, Hono } from "hono";
 import { cors } from "hono/cors";
 
 import { schema } from "generated/graphql/schema.executable";
@@ -16,9 +16,14 @@ import {
   isProdEnv,
 } from "lib/config/env";
 import { createGraphQLContext } from "lib/graphql/context";
+import { authMiddleware } from "lib/middleware";
 import { useAuth } from "lib/plugins";
 
 // TODO run on Bun runtime instead of Node, track https://github.com/oven-sh/bun/issues/11785
+
+interface Variables {
+  customerId: string;
+}
 
 /**
  * GraphQL Yoga configuration.
@@ -34,7 +39,7 @@ const yoga = createYoga({
   plugins: [useAuth(), useMoreDetailedErrors(), useGrafast()],
 });
 
-const app = new Hono();
+const app = new Hono<{ Variables: Variables }>();
 
 app.use(
   // enable CORS
@@ -47,6 +52,7 @@ app.use(
 
 app.get(
   "/checkout",
+  authMiddleware,
   Checkout({
     accessToken: POLAR_ACCESS_TOKEN!,
     successUrl: SUCCESS_URL!,
@@ -56,10 +62,12 @@ app.get(
 
 app.get(
   "/portal",
+  authMiddleware,
   CustomerPortal({
     accessToken: POLAR_ACCESS_TOKEN!,
-    // TODO: update
-    getCustomerId: (_event) => new Promise((resolve) => resolve("")),
+    // biome-ignore lint/complexity/noBannedTypes: match Hono type
+    getCustomerId: (c: Context<{ Variables: Variables }, "/portal", {}>) =>
+      new Promise(() => c.var.customerId),
     server: isDevEnv ? "sandbox" : "production",
   })
 );
