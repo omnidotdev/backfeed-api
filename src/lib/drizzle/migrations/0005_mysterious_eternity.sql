9bf0d9c62d49fbb9e6d13686cd6d3bec597225d7
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-02-18 14:47:40 -0600

    chore: prep merge

--- src/lib/drizzle/migrations/0005_mysterious_eternity.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "user" ADD COLUMN "customer_id" uuid;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "product_id" uuid;--> statement-breakpoint
-ALTER TABLE "user" ADD CONSTRAINT "user_customerId_unique" UNIQUE("customer_id");
