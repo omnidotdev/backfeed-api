77827c95b8de2dfc70c07e2f016ca9d5b1bd61f3
Brian Cooper <brian@brian-cooper.com>
2025-03-26 16:46:38 -0500

    chore(graphile): update exec schema

--- src/generated/graphql/schema.executable.ts
+++ src/generated/graphql/schema.executable.ts
@@ -148,7 +148,7 @@ const spec_downvote = {
   }),
   description: undefined,
   extensions: {
-    oid: "177582",
+    oid: "40971",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -228,7 +228,7 @@ const spec_upvote = {
   }),
   description: undefined,
   extensions: {
-    oid: "177495",
+    oid: "40884",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -308,7 +308,7 @@ const spec_organization = {
   }),
   description: undefined,
   extensions: {
-    oid: "177457",
+    oid: "40846",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -400,7 +400,7 @@ const spec_comment = {
   }),
   description: undefined,
   extensions: {
-    oid: "177562",
+    oid: "40951",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -504,7 +504,7 @@ const spec_user = {
   }),
   description: undefined,
   extensions: {
-    oid: "177505",
+    oid: "40894",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -523,7 +523,7 @@ const roleCodec = enumCodec({
   values: ["owner", "admin", "member"],
   description: undefined,
   extensions: {
-    oid: "177601",
+    oid: "40990",
     pg: {
       serviceName: "main",
       schemaName: "public",
@@ -599,7 +599,7 @@ const spec_member = {
   }),
   description: undefined,
   extensions: {
-    oid: "177517",
+    oid: "40906",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -727,7 +727,7 @@ const spec_post = {
   }),
   description: undefined,
   extensions: {
-    oid: "177471",
+    oid: "40860",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -843,7 +843,7 @@ const spec_postStatus = {
   }),
   description: undefined,
   extensions: {
-    oid: "177655",
+    oid: "41045",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -959,7 +959,7 @@ const spec_project = {
   }),
   description: undefined,
   extensions: {
-    oid: "177481",
+    oid: "40870",
     isTableLike: true,
     pg: {
       serviceName: "main",
