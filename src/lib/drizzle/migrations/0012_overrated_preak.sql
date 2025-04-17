d70446b3c637e8027f4e5aac6e01b9e147e7b0d7
Brian Cooper <brian@brian-cooper.com>
2025-04-17 18:52:27 -0500

    chore(db,organization): disable auto-generated slug columns, add TODO

---
+++ src/lib/drizzle/migrations/0012_overrated_preak.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "organization" ALTER COLUMN "slug" DROP EXPRESSION;
--- src/lib/drizzle/schema/organization.table.ts
+++ src/lib/drizzle/schema/organization.table.ts
@@ -1,7 +1,7 @@
 import { pgTable, text, uniqueIndex } from "drizzle-orm/pg-core";
 
 import { defaultDate, defaultId } from "./constants";
-import { generateSlug } from "./helpers";
+// import { generateSlug } from "./helpers";
 
 import type { InferInsertModel, InferSelectModel, SQL } from "drizzle-orm";
 
@@ -14,13 +14,14 @@ export const organizations = pgTable(
     id: defaultId(),
     name: text().unique().notNull(),
     slug: text()
-      .generatedAlwaysAs((): SQL => generateSlug(organizations.name))
+      // TODO https://linear.app/omnidev/issue/69c6f70e-0821-4a3a-a04a-971547f29690
+      // .generatedAlwaysAs((): SQL => generateSlug(organizations.name))
       .unique()
       .notNull(),
     createdAt: defaultDate(),
     updatedAt: defaultDate(),
   },
-  (table) => [uniqueIndex().on(table.id), uniqueIndex().on(table.slug)]
+  (table) => [uniqueIndex().on(table.id), uniqueIndex().on(table.slug)],
 );
 
 /**
