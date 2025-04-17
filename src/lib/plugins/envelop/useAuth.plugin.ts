67dabdb5fa4df4c6031888de358edffea23a33a9
Brian Cooper <brian@brian-cooper.com>
2025-04-17 11:15:47 -0500

    chore(use-auth): update TODOs

--- src/lib/plugins/envelop/useAuth.plugin.ts
+++ src/lib/plugins/envelop/useAuth.plugin.ts
@@ -1,5 +1,5 @@
 import { useGenericAuth } from "@envelop/generic-auth";
-import type * as jose from "jose";
+import * as jose from "jose";
 
 import { AUTH_BASE_URL } from "lib/config/env.config";
 import { users } from "lib/drizzle/schema";
@@ -7,6 +7,8 @@ import { users } from "lib/drizzle/schema";
 import type { ResolveUserFn } from "@envelop/generic-auth";
 import type { InsertUser, SelectUser } from "lib/drizzle/schema";
 import type { GraphQLContext } from "lib/graphql";
+
+// TODO research best practices for all of this file (token validation, caching, etc.). Validate access token (introspection endpoint)? Cache userinfo output? etc. (https://linear.app/omnidev/issue/OMNI-302/increase-security-of-useauth-plugin)
 
 /**
  * Validate user session and resolve user if successful.
@@ -16,25 +18,29 @@ const resolveUser: ResolveUserFn<SelectUser, GraphQLContext> = async (
   context,
 ) => {
   try {
-    const sessionToken = context.request.headers
+    const accessToken = context.request.headers
       .get("authorization")
       ?.split("Bearer ")[1];
 
-    if (!sessionToken) throw new Error("Invalid or missing session token");
+    if (!accessToken) throw new Error("Invalid or missing access token");
 
-    // TODO verify best practices for this
+    // TODO validate access token (introspection endpoint?) here?
+
+    // TODO cache so this doesn't occur on every request. Research best practices
     const userInfo = await fetch(`${AUTH_BASE_URL}/oauth2/userinfo`, {
       headers: {
-        Authorization: `Bearer ${sessionToken}`,
+        Authorization: `Bearer ${accessToken}`,
       },
     });
 
+    if (!userInfo.ok) throw new Error("Invalid access token or request failed");
+
     const idToken: jose.JWTPayload = await userInfo.json();
 
-    // TODO validate token, currently major security flaw
+    // TODO validate token, currently major security flaw (pending BA OIDC JWKS support: https://www.better-auth.com/docs/plugins/oidc-provider#jwks-endpoint-not-fully-implemented) (https://linear.app/omnidev/issue/OMNI-302/validate-id-token-with-jwks)
     // const jwks = jose.createRemoteJWKSet(new URL(`${AUTH_BASE_URL}/jwks`));
-    // const { payload } = await jose.jwtVerify(sessionToken, jwks);
-    // if (!payload) throw new Error("Invalid or missing session token");
+    // const { payload } = await jose.jwtVerify(JSON.stringify(idToken), jwks);
+    // if (!payload) throw new Error("Failed to verify token");
 
     const insertedUser: InsertUser = {
       hidraId: idToken.sub!,
