a3f223e53728ac09ebb2fb4474a1cf648d946a59
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-05-15 09:41:34 -0500

    refactor(plugins): add error logs to upvote downvote mutations

--- src/generated/graphql/schema.executable.ts
+++ src/generated/graphql/schema.executable.ts
@@ -154,7 +154,7 @@ const spec_downvote = {
   },
   description: undefined,
   extensions: {
-    oid: "230048",
+    oid: "230322",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -237,7 +237,7 @@ const spec_upvote = {
   },
   description: undefined,
   extensions: {
-    oid: "229961",
+    oid: "230235",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -320,7 +320,7 @@ const spec_invitation = {
   },
   description: undefined,
   extensions: {
-    oid: "230147",
+    oid: "230421",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -403,7 +403,7 @@ const spec_organization = {
   },
   description: undefined,
   extensions: {
-    oid: "229923",
+    oid: "230197",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -510,7 +510,7 @@ const spec_comment = {
   },
   description: undefined,
   extensions: {
-    oid: "230028",
+    oid: "230302",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -629,7 +629,7 @@ const spec_project = {
   },
   description: undefined,
   extensions: {
-    oid: "229947",
+    oid: "230221",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -650,7 +650,7 @@ const roleCodec = enumCodec({
   values: ["owner", "admin", "member"],
   description: undefined,
   extensions: {
-    oid: "230067",
+    oid: "230341",
     pg: {
       serviceName: "main",
       schemaName: "public",
@@ -729,7 +729,7 @@ const spec_member = {
   },
   description: undefined,
   extensions: {
-    oid: "229983",
+    oid: "230257",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -860,7 +860,7 @@ const spec_post = {
   },
   description: undefined,
   extensions: {
-    oid: "229937",
+    oid: "230211",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -979,7 +979,7 @@ const spec_postStatus = {
   },
   description: undefined,
   extensions: {
-    oid: "230121",
+    oid: "230395",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -1000,7 +1000,7 @@ const tierCodec = enumCodec({
   values: ["free", "basic", "team", "enterprise"],
   description: undefined,
   extensions: {
-    oid: "230170",
+    oid: "230444",
     pg: {
       serviceName: "main",
       schemaName: "public",
@@ -1129,7 +1129,7 @@ const spec_user = {
   },
   description: undefined,
   extensions: {
-    oid: "229971",
+    oid: "230245",
     isTableLike: true,
     pg: {
       serviceName: "main",
@@ -4326,7 +4326,12 @@ const planWrapper7 = (plan, _, fieldArgs) => {
         downvotes
       } = lib_drizzle_schema,
       [downvote] = await db.select().from(downvotes).where(eq(downvotes.id, downvoteId));
-    if (currentUser.id !== downvote.userId) throw new Error("Insufficient permissions");
+    if (currentUser.id !== downvote.userId) {
+      console.error("Current User ID", currentUser.id);
+      console.error("Downvote User ID", downvote.userId);
+      console.error("Downvote Row ID", downvote.id);
+      throw new Error("Insufficient permissions");
+    }
   });
   return plan();
 };
@@ -4349,7 +4354,12 @@ const planWrapper8 = (plan, _, fieldArgs) => {
         upvotes
       } = lib_drizzle_schema,
       [upvote] = await db.select().from(upvotes).where(eq(upvotes.id, upvoteId));
-    if (currentUser.id !== upvote.userId) throw new Error("Insufficient permissions");
+    if (currentUser.id !== upvote.userId) {
+      console.error("Current User ID", currentUser.id);
+      console.error("Upvote User ID", upvote.userId);
+      console.error("Upvote Row ID", upvote.id);
+      throw new Error("Insufficient permissions");
+    }
   });
   return plan();
 };
@@ -4646,7 +4656,12 @@ const planWrapper16 = (plan, _, fieldArgs) => {
         downvotes
       } = lib_drizzle_schema,
       [downvote] = await db.select().from(downvotes).where(eq(downvotes.id, downvoteId));
-    if (currentUser.id !== downvote.userId) throw new Error("Insufficient permissions");
+    if (currentUser.id !== downvote.userId) {
+      console.error("Current User ID", currentUser.id);
+      console.error("Downvote User ID", downvote.userId);
+      console.error("Downvote Row ID", downvote.id);
+      throw new Error("Insufficient permissions");
+    }
   });
   return plan();
 };
@@ -4669,7 +4684,12 @@ const planWrapper17 = (plan, _, fieldArgs) => {
         upvotes
       } = lib_drizzle_schema,
       [upvote] = await db.select().from(upvotes).where(eq(upvotes.id, upvoteId));
-    if (currentUser.id !== upvote.userId) throw new Error("Insufficient permissions");
+    if (currentUser.id !== upvote.userId) {
+      console.error("Current User ID", currentUser.id);
+      console.error("Upvote User ID", upvote.userId);
+      console.error("Upvote Row ID", upvote.id);
+      throw new Error("Insufficient permissions");
+    }
   });
   return plan();
 };
