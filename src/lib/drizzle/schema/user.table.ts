389057f28cec544754a0d9630a477e867e397d0e
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-26 10:27:25 -0600

    feature(schema): add user relations

--- src/lib/drizzle/schema/user.table.ts
+++ src/lib/drizzle/schema/user.table.ts
@@ -1,6 +1,12 @@
+import { relations } from "drizzle-orm";
 import { pgTable, text, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
+import { comments } from "./comment.table";
 import { defaultDate, defaultId } from "./constants";
+import { downvotes } from "./downvote.table";
+import { members } from "./member.table";
+import { posts } from "./post.table";
+import { upvotes } from "./upvote.table";
 
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
 
@@ -23,7 +29,16 @@ export const users = pgTable(
   (table) => [uniqueIndex().on(table.id), uniqueIndex().on(table.hidraId)],
 );
 
-// TODO: user relations?
+/**
+ * User relations.
+ */
+export const userRelations = relations(users, ({ many }) => ({
+  memberships: many(members),
+  comments: many(comments),
+  downvotes: many(downvotes),
+  upvotes: many(upvotes),
+  posts: many(posts),
+}));
 
 /**
  * Type helpers related to the user table.
