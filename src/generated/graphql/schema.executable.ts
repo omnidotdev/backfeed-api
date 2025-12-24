a9a6fd1fa252de107a3d848674795dbf4585956d
Brian Cooper <brian@brian-cooper.com>
2025-12-24 00:27:11 -0800

    chore(graphql): inject serialized billing bypass slug

--- src/generated/graphql/schema.executable.ts
+++ src/generated/graphql/schema.executable.ts
@@ -4774,7 +4774,7 @@ function oldPlan3(_, args) {
     result: $insert
   });
 }
-const billingBypassSlugs = [];
+const billingBypassSlugs = ["omni"];
 const planWrapper3 = (plan, _, fieldArgs) => {
   const $input = fieldArgs.getRaw(["input", "comment"]),
     $observer = context().get("observer"),
